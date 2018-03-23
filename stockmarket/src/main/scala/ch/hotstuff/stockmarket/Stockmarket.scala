7228fc58adf7de404a91c87eac4a374ec550b46d
Marius Kleiner <marius.kleiner@adnovum.ch>
2018-03-23 10:17:13 +0100

    Initial implementation of maxProfit function

--- stockmarket/src/main/scala/ch/hotstuff/stockmarket/Stockmarket.scala
+++ stockmarket/src/main/scala/ch/hotstuff/stockmarket/Stockmarket.scala
@@ -3,5 +3,23 @@ package ch.hotstuff.stockmarket
 object Stockmarket extends App {
   println("Hello, world!")
   
-  def maximumProfit(stock : List[Int]) : Int = 0
+  def maximumProfit(stock : List[Int]) : Int = {
+    def maximumProfitRec(idx: Int, absMinIdx: Int, buyIdx: Int, sellIdx: Int) : Int =
+      if (idx == stock.length) stock(sellIdx) - stock(buyIdx)
+      else {
+        val currMaxProfit = stock(sellIdx) - stock(buyIdx)
+        val sellNowProfit = stock(idx) - stock(buyIdx)
+        val newMinProfit = stock(idx) - stock(absMinIdx)
+        val newAbsMinIdx = if (stock(idx) < stock(absMinIdx)) idx else absMinIdx
+        
+        if (currMaxProfit > sellNowProfit && currMaxProfit > newMinProfit)
+          maximumProfitRec(idx + 1, newAbsMinIdx, buyIdx, sellIdx)
+        else if (sellNowProfit > currMaxProfit && sellNowProfit > newMinProfit)
+          maximumProfitRec(idx + 1, newAbsMinIdx, buyIdx, idx)
+        else // newMinProfit is largest
+          maximumProfitRec(idx + 1, newAbsMinIdx, absMinIdx, idx)
+      }
+    
+    maximumProfitRec(0, 0, 0, 0)
+  }
 }
